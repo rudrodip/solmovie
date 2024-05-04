5be81f98fa6aedd4ea6bdc6a3c8b322f82fe2d0a
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-04 11:17:24 +0600

    feat: Add MovieList component for displaying a list of movies

---
+++ src/components/movie/movie-list.tsx
@@ -0,0 +1,20 @@
+export default function MovieList() {
+  return (
+    <section className="w-full flex flex-col gap-3">
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+    </section>
+  )
+}
