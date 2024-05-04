45ececd6122c868a7c85a979ce36f257a32aa48a
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-04 11:17:08 +0600

    Add Header component to RootLayout

--- src/root.tsx
+++ src/root.tsx
@@ -6,6 +6,7 @@ import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import * as web3 from "@solana/web3.js";
 import { useMemo } from "react";
 import '@solana/wallet-adapter-react-ui/styles.css'
+import Header from "@/components/header";
 
 type RootLayoutProps = {
   children: React.ReactNode;
@@ -19,7 +20,10 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets}>
         <WalletModalProvider>
-          {children}
+          <main className="w-full h-screen overflow-hidden flex-1">
+            <Header />
+            {children}
+          </main>
         </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
