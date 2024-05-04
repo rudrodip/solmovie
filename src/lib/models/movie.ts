881af6f90b845c850907244abb685714115f50d1
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-04 13:34:09 +0600

    feat: Add deserialize method to Movie model for deserializing movie data

--- src/lib/models/movie.ts
+++ src/lib/models/movie.ts
@@ -19,9 +19,30 @@ export class Movie {
     borsh.str("description"),
   ]);
 
+  static borshAccountSchema = borsh.struct([
+    borsh.bool('initialized'),
+    borsh.u8('rating'),
+    borsh.str('title'),
+    borsh.str('description'),
+  ])
+
   serialize(): Buffer {
     const buffer = Buffer.alloc(1000);
     this.borshInstructionSchema.encode({ ...this, variant: 0 }, buffer);
     return buffer.subarray(0, this.borshInstructionSchema.getSpan(buffer));
+  }
+
+  static deserialize(buffer?: Buffer): Movie|null {
+    if (!buffer) {
+      return null
+    }
+
+    try {
+      const { title, rating, description } = this.borshAccountSchema.decode(buffer)
+      return new Movie(title, rating, description)
+    } catch(error) {
+      console.log('Deserialization error:', error)
+      return null
+    }
   }
 }
